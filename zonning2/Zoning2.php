a2f67df8589bcab085412af360bddc39548dcab1
Greg <gregorylyvonet@lepoles.org>
2024-05-23 17:10:49 +0200

    cours php

---
+++ zonning2/Zoning2.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>exercices2</title>
+
+    <link rel="stylesheet" href="public/assets/css/zoning2.css">
+
+</head>
+
+<body>
+
+<div id="content">
+
+    <header>zone du haut</header>
+
+    <nav>menu</nav>
+
+    <main>
+        <section class="sec1">
+            <div>Petite Zone 1</div>
+            <div>Petite Zone 2</div>
+            <div>Petite Zone 3</div>
+            <div>Petite Zone 4</div>
+        </section>
+        
+        <section class="sec2">
+            <div>Petite Zone 5</div>
+            <div>Petite Zone 6</div>
+            <div>Petite Zone 7</div>
+            <div>Petite Zone 8</div>
+        </section>
+        
+        <section class="sec3">
+            <div>Petite Zone 9</div>
+            <div>Petite Zone 10</div>
+            <div>Petite Zone 11</div>
+            <div>Petite Zone 12</div>
+        </section>
+        
+        <section class="sec4">
+            <div>Petite Zone 13</div>
+            <div>Petite Zone 14</div>
+            <div>Petite Zone 15</div>
+            <div>Petite Zone 16</div>
+       
+        </section>
+    
+   
+    </main>
+        
+        <footer>Zone du bas</footer>
+
+</div>
+
+
+
+
+
+    
+</body>
+</html>
---
+++ zonning2/public/header.html.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <base href="http://localhost/court_html/zonning2/">
+    <link rel="stylesheet" href="public/assets/css/styles.css">
+
+    <title>Document</title>
+
+</head>
+
+<body>
+
+  <div id="content">
+
+    <header>Haut</header>
+
+<?php include __DIR__ . "/nav.html.php"; ?>
